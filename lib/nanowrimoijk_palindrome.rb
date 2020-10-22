ec32209303003146310e5bc51276c7245264d13a
Nanowrimoijk <Nanowrimoijk@gmail.com>
2020-10-21 20:09:47 -0600

    Finish working and refactoring palindrome method

--- lib/nanowrimoijk_palindrome.rb
+++ lib/nanowrimoijk_palindrome.rb
@@ -1,6 +1,15 @@
 require "nanowrimoijk_palindrome/version"
 
-module NanowrimoijkPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+  def processed_content
+    scan(/[a-z]/i).join.downcase
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,7 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "nanowrimoijk_palindrome"
 
 require "minitest/autorun"
+
+require "minitest/reporters"
+
+Minitest::Reporters.use!
